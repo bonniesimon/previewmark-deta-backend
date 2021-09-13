3aa17fd5730e079dd167c5b1c0ffa52c56486291
Bonnie Simon <bonnie.developer@gmail.com>
2021-09-13 19:34:52 +0530

    Add deta driver code for deta base.
    Trying to access detaBase from inside detaMicro.

--- index.js
+++ index.js
@@ -2,8 +2,23 @@
 const express = require('express')
 const app = express()
 
-app.get('/', (req, res) => res.send('Hello World!'))
+const {Deta} = require('deta');
 
+const deta = Deta('a036obeb');
+const db = deta.Base("previewmark");
+
+const date = Date.now().toString();
+
+
+app.get('/', async (req, res) => {
+	// const value = await db.insert({string: "hey this is the string", date: date});
+	// console.log(value);
+	res.send('Hello World!')}
+)
+
+app.listen(3001, ()=>{
+	console.log("Server running successfully");
+})
 /**
  * NO NEED FOR app.listen
  * Deta Micros takes care of it.
