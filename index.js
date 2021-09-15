c98375a2dfba91bd3e92cc058e019b698e70007f
Bonnie Simon <bonnie.developer@gmail.com>
2021-09-15 10:22:13 +0530

    Add dotenv. Add deta project key. Add morgan for logging.
    The deta project key can be obtained by going to dashboard in deta and
    then settings then creating a new project key.

--- index.js
+++ index.js
@@ -1,23 +1,35 @@
 // install express with `npm install express` 
 const express = require('express')
 const app = express()
+require('dotenv').config();
+const morgan = require('morgan');
+
+/**
+ * Setting up the logger
+ */
+app.use(morgan(':method :url :status :response-time ms - :res[content-length]'));
 
 const {Deta} = require('deta');
 
-const deta = Deta('a036obeb');
+const deta = Deta(process.env.DETA_PROJECT_KEY);
 const db = deta.Base("previewmark");
 
 const date = Date.now().toString();
 
 
 app.get('/', async (req, res) => {
-	// const value = await db.insert({string: "hey this is the string", date: date});
-	// console.log(value);
+	try {
+		// const value = await db.insert({string: "hey this is the string", date: date});
+		// console.log(value);
+		console.log("Hey");
+	} catch (error) {
+		console.log(error);	
+	}
 	res.send('Hello World!')}
 )
 
-app.listen(3001, ()=>{
-	console.log("Server running successfully");
+app.listen(process.env.DEV_PORT, ()=>{
+	console.log(`Server running successfully @ localhost:${process.env.DEV_PORT}`);
 })
 /**
  * NO NEED FOR app.listen
